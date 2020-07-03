43de1371773ea9140cc47da9ac4c3823e6f5227e
marco-beduschi <je-0n-r0g-fea>
2020-07-03 15:02:58 -0300

    tweak variables

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
-export const TICK_RATE = 1000;
+export const TICK_RATE = 3000;
 export const ICONS = ["fish", "poop", "weather"];
-export const RAIN_CHANCE = 0.2;
+export const RAIN_CHANCE = 0.4;
 export const SCENES = ["day", "rain"];
 export const DAY_LENGTH = 60;
 export const NIGHT_LENGTH = 3;
@@ -15,10 +15,10 @@ export const getNextHungerTime = (clock) => {
   return Math.floor(Math.random() * 3) + 5 + clock;
 };
 export const getNextDieTime = (clock) => {
-  return Math.floor(Math.random() * 2) + 3 + clock;
+  return Math.floor(Math.random() * 2) + 12 + clock;
 };
 export const getNextPoopTime = (clock) => {
-  return Math.floor(Math.random() * 2) + 6 + clock;
+  return Math.floor(Math.random() * 2) + 8 + clock;
 };
 
 export const writeModal = (text = "") => {
